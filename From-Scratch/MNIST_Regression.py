adc1e3033fb61a15b7b614b4afed78b6d2610e33
Massimo Ginella <massimoginella12@gmail.com>
2025-04-06 16:48:36 -0700

    added documentation

--- From-Scratch/MNIST_Regression.py
+++ From-Scratch/MNIST_Regression.py
@@ -1,39 +1,78 @@
+# *************************** PROGRAM EXPLANATION AND AUTHOR *************************** #
+# This is a convolutional neural network designed to recognize hand drawn digits on a    #
+# 28x28 pixel grid. It uses the MNIST Database which is comprised of tens of thousands   #
+# of labeled hand drawn digits. This network has 4 activation layers: 1 input layer, 2   #
+# hidden layers, and 1 output layer. This Network does not use pytorch, keras, or        #
+# tensorflow. Just numpy, multivariable calculus, and linear algebra.                    #
+#                                                                                        #
+# Author: Massimo Ginella                                                                #
+# Email: massimoginella12@gmail.com                                                      #
+# ************************************************************************************** #
+
+
+# ********************************** MORE INFORMATION ********************************** #
+# This code is largely inspired by '3Blue1Brown' and 'Samson Zhang' on YouTube           #
+#                                                                                        #
+# 3Blue1Brown's Video: https://www.youtube.com/watch?v=aircAruvnKk                       #                                           
+# Samson Zhang's Video: https://www.youtube.com/watch?v=w8yWXqWQYmU&t=668s               #
+# ************************************************************************************** #
+
+
+# Import Dependencies
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
+
+
 class MNIST_Regression:
 
-    layer_0_size = 0                           # input layer has 784 neurons (one for each pixel)
-    layer_1_size = 30                          # hidden layer 1 has 16 neurons
-    layer_2_size = 30                          # hidden layer 2 has 16 neurons
-    layer_3_size = 10                          # output layer has 10 neurons (one for each predicted digit 1 - 9)
+    # ********************************************* #
+    # CUSTOMIZE NEURAL NETWORK SPECIFICATIONS HERE: #
+    # ********************************************* #
 
-    learning_ratio = 0.5                       # ratio for the rate at which the model learns
+    # Neural network neuron count in each layer layer_0 should always = 0, layer_3 should always = 10. Only change layer_1 and layer_2
+    layer_0_size = 0
+    layer_1_size = 30
+    layer_2_size = 30
+    layer_3_size = 10
 
-    validation_end = 1000                      # how many data examples will be used for validation (0 -> validation_end)
+    # Ratio for the rate at which the model learns
+    learning_ratio = 0.5
 
-    training_start = 1000                      # starting point for data samples used for training (training_start -> m)
+    # Data samples used for validation (0 -> validation_end)
+    validation_end = 10000
 
-    datafile_name = 'train.csv.zip'            # name of the input training and validation data file
+    # Starting point for data samples used for training (training_start -> m)
+    training_start = 10000
 
+    # Name of input file containing training and validation data
+    datafile_name = 'train.csv.zip'
+
+    # Number of times the training data will pass through the Neural Network
     num_epochs = 1000
 
 
 
+    # Initializes the MNIST_Regression class
     def __init__(self):
 
+        # Load data into respective training and labeled data
         X_validation, Y_validation, X_training, Y_training = self.load_data()
 
+        # Set the number of neurons in the first layer to the w x h pixel dimension of input img
         MNIST_Regression.layer_0_size = X_validation.shape[0]
 
-        # TRAINING AND VALIDATION
+        # Run the neural network with the aquired training and label data
         self.gradient_descent(X_validation, Y_validation, X_training, Y_training, MNIST_Regression.num_epochs)
 
 
 
+    # Gradient descent function runs forward propogation, back propogation and updates the parameters (weights and biases) of the network
     def gradient_descent(self, X_val, Y_val, X_train, Y_train, epochs):
+
+        # Initialize the amount of training samples (m) and sizes of the activation layers, weights, biases, and weighted sums based upon it
         m = X_train.shape[1] 
         activations, weights, biases, weighted_sums = self.init_neural_net(m)
         weight_nudges, bias_nudges, weighted_sum_changes = self.init_nudges(m)
@@ -59,7 +98,6 @@ class MNIST_Regression:
                 print("")
 
 
-            # I think its problematic to have the weights, weighted_sums, and biases change during validation??
             validation_activations, validation_weighted_sums = self.forward_propogration(X_val, activations, weights, weighted_sums, biases)
             validation_predictions, validation_actual, validation_accuracy = self.get_accuracy(self.get_predictions(activations[len(activations) - 1]), Y_val)
             validation_accuracy = validation_accuracy * 100 
@@ -74,6 +112,7 @@ class MNIST_Regression:
 
 
 
+    #  Initializes the sizes of the activation, weight, bias, and weighted sum matrices
     def init_neural_net (self, m):
         # Create matrices for each layer of the Neural Network
         A_0 = np.zeros((MNIST_Regression.layer_0_size, m))
@@ -111,6 +150,7 @@ class MNIST_Regression:
 
 
 
+    # Initializes the size of the weight, bias, and weighted sum nudge matrices for param updating
     def init_nudges(self, m):
         # Create matrices that contain nudges to add to weights between layers
         dW_1 = np.empty((MNIST_Regression.layer_1_size, MNIST_Regression.layer_0_size))
@@ -140,9 +180,11 @@ class MNIST_Regression:
 
 
 
+    # Loads data 
     def load_data(self):
+        # load in training data (each training sample is a row that is 785 column long where column 1 = labeled digit, column 2 -> column 785 = each pixel)
         print("Fetching Data...")
-        datafile = pd.read_csv(MNIST_Regression.datafile_name)   # load in training data (each training sample is a row that is 785 columns long (col 1 = expected digit, col2 -> 785 is each pixel))
+        datafile = pd.read_csv(MNIST_Regression.datafile_name)
         data = np.array(datafile)
         np.random.shuffle(data)
         m, n = data.shape
@@ -181,31 +223,40 @@ class MNIST_Regression:
         return activation_layers, weighted_sums
 
 
-
+    # Converts raw data into probabilities 
     def softmax(Z):
         A = np.exp(Z) / sum(np.exp(Z))
         return A
 
 
 
-    # Sigmoid function squishes all the weighted sums between 0 - 1 for compatibility
-    # with neuron activation layers
+    # Squishes all the weighted sums between 0 & 1
     def sigmoid(self, Z):
         Z = np.clip(Z, -600, 600)     # to prevent overflow
         return 1/(1 + np.exp(-Z))
 
 
+    
+    # Derivative of sigmoid function
+    def deriv_sigmoid(self, Z):
+        sigmoid = self.sigmoid(Z)
+        return (sigmoid * (1 - sigmoid))
 
+
+
+    # Sets weighted sums to either 0 or 1
     def ReLU(self, Z):
         return np.maximum(Z, 0)
 
 
 
+    # Derivative of ReLU function
     def ReLU_deriv(self, Z):
         return Z > 0
 
 
 
+    # Calculates the nudges needed to be added to the weights and biases of the neural network in order to optimize it for a certian input pixel pattern
     def back_propogration(self, Y, samples, activation_layers, weights, weighted_sums, weighted_sum_changes, weight_nudges, bias_nudges):
         one_hot_Y = self.one_hot(Y)
         i = len(weighted_sum_changes) - 1
@@ -222,15 +273,10 @@ class MNIST_Regression:
 
         return weighted_sum_changes, weight_nudges, bias_nudges
 
-    
-
-    def deriv_sigmoid(self, Z):
-        sigmoid = self.sigmoid(Z)
-        return (sigmoid * (1 - sigmoid))
 
     
 
-    # take the matrix of correct answers and change each column element to be zero except for the correct answer which will be 1
+    # Takes the matrix of labeled data and changes each column element to be zero except for the correct answer which will be 1
     def one_hot(self, Y):
         one_hot_Y = np.zeros((Y.size, Y.max() + 1))              # fill one_hot_Y with zeros and change its size to Y.size = m rows and Y.max() + 1= 9 + 1 = 10 columns 
         one_hot_Y[np.arange(Y.size), Y] = 1                      # index through one_hot_Y by accessing row np.arange(Y.size) which is an array from 0 - m, and column Y which is a value from 0 - 9, and set it to 1
@@ -239,6 +285,7 @@ class MNIST_Regression:
 
 
 
+    # Updates the weights and biases of the neural network based upon the weight and bias nudges calculated in back propogation
     def update_parameters(self, weights, weight_nudges, biases, bias_nudges):
         for i in range(len(weights)):
             weights[i] = weights[i] - MNIST_Regression.learning_ratio * weight_nudges[i]
@@ -247,18 +294,19 @@ class MNIST_Regression:
 
     
 
+    # Calculates the accuracy of the neural networks predictions in the final output layer (also returns a slice of the predictiona and labels for visualization)
     def get_accuracy(self, predictions, Y):
         return predictions[0 : 30], Y[0 : 30], np.sum(predictions == Y) / Y.size
 
 
 
+    # Takes raw output layer neurons and creates a numpy array of each numerical guess from the ouput layer data
     def get_predictions(self, output_layer):
         return np.argmax(output_layer, 0)
 
 
-    # This function plots relevant data at the end of training such as
-    # accuracy improvements over time, final accuracy scores, and visualizations
-    # of the neural networks correct and incorrect predictions
+
+    # Plots relevant data at the end of training such as accuracy improvements over time, final accuracy scores, and visualizations of the neural networks correct and incorrect predictions
     def plot(self, validation_accuracy_scores, training_accuracy_scores, epochs, X_val, Y_val, validation_activations):
         fig1, ax = plt.subplots(2, 2, figsize = (12, 8))
 
@@ -269,7 +317,7 @@ class MNIST_Regression:
         ax[0 , 0].set_xlim(0, MNIST_Regression.num_epochs)
         ax[0 , 0].xaxis.set_major_locator(MultipleLocator(100))
         ax[0 , 0].yaxis.set_major_locator(MultipleLocator(10))
-        ax[0 , 0].set_title('Neural Net Accuracy')
+        ax[0 , 0].set_title('Neural Network Accuracy:')
         ax[0 , 0].set_xlabel('Epochs')
         ax[0 , 0].set_ylabel('Accuracy')
         ax[0 , 0].legend()
@@ -277,11 +325,13 @@ class MNIST_Regression:
         # Plot the final accuracy values for the training and validation processeses
         categories = ['Validation', 'Training']
         data = [validation_accuracy_scores[-1], training_accuracy_scores[-1]]
-        ax[1, 0].bar(categories, data)
-        ax[1, 0].set_ylim(0, 100)
-        ax[1, 0].yaxis.set_major_locator(MultipleLocator(10))
-        ax[1, 0].set_title('Validation vs Training Accuracy')
-        ax[1, 0].set_ylabel('Final Accuracy Scores')
+        ax[1 , 0].bar(categories, data)
+        ax[1 , 0].set_ylim(0, 100)
+        ax[1 , 0].yaxis.set_major_locator(MultipleLocator(10))
+        ax[1 , 0].set_title('Validation vs Training Accuracy')
+        ax[1 , 0].set_ylabel('Final Accuracy Scores')
+        ax[1 , 0].text(.9, 93, f'{training_accuracy_scores[-1]:.2f}%', fontsize = 12)
+        ax[1 , 0].text(-.08, 93, f'{validation_accuracy_scores[-1]:.2f}%', fontsize = 12)
 
 
         # Plot a visualization of a correct prediction
@@ -298,10 +348,10 @@ class MNIST_Regression:
         label = Y_val[correct_prediction]
         current_image = current_image.reshape((28, 28)) * 255
 
-        ax[0, 1].set_title('Correct Prediction Example:', color='green')
-        ax[0, 1].imshow(current_image, cmap = 'gray', interpolation='nearest')
-        ax[0, 1].text(0, 1, f'Actual Number: {label}', color='white', fontsize=12)
-        ax[0, 1].text(0, 2.5, f'Predicted Number: {prediction}', color='green', fontsize=12)
+        ax[0 , 1].set_title('Correct Prediction Example:', color='green')
+        ax[0 , 1].imshow(current_image, cmap = 'gray', interpolation='nearest')
+        ax[0 , 1].text(0, 1, f'Actual Number: {label}', color='white', fontsize = 12)
+        ax[0 , 1].text(0, 2.5, f'Predicted Number: {prediction}', color='green', fontsize = 12)
 
         
         # Plot a visualization of an incorrect prediction
@@ -316,10 +366,10 @@ class MNIST_Regression:
         label = Y_val[incorrect_prediction]
         current_image = current_image.reshape((28, 28)) * 255
         
-        ax[1, 1].set_title('Incorrect Prediction Example:', color='red')
-        ax[1, 1].imshow(current_image, cmap = 'gray', interpolation='nearest')
-        ax[1, 1].text(0, 1, f'Actual Number: {label}', color='white', fontsize=12)
-        ax[1, 1].text(0, 2.5, f'Predicted Number: {prediction}', color='red', fontsize=12)
+        ax[1 , 1].set_title('Incorrect Prediction Example:', color='red')
+        ax[1 , 1].imshow(current_image, cmap = 'gray', interpolation='nearest')
+        ax[1 , 1].text(0, 1, f'Actual Number: {label}', color='white', fontsize=12)
+        ax[1 , 1].text(0, 2.5, f'Predicted Number: {prediction}', color='red', fontsize=12)
 
         fig1.tight_layout()
 
--- From-Scratch/main.py
+++ From-Scratch/main.py
@@ -1,3 +1,8 @@
+# ********** HOW TO RUN NEURAL NETWORK: ********** #
+# In terminal window, type: python3 main.cpp       #
+# and press enter.                                 #
+# ************************************************ #
+
 from MNIST_Regression import MNIST_Regression
 
 Run_AI = MNIST_Regression()
